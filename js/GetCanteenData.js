954545067defaa428665d2c4c7c49098e9bf0b8e
LogCreative <lizilong123456@live.cn>
2020-08-24 18:00:36 +0800

    RecommendAlg II

--- js/GetCanteenData.js
+++ js/GetCanteenData.js
@@ -21,11 +21,40 @@ function refreshData(){
                 var remain = 0;
 
                 // 距离数据
+                // 一 二 三 四 五 六 七 哈乐 玉兰
                 var dist = [0.55,1.0,0.75,0.15,1.6,1.7,1.5,0.77,0.41];
+
+                var totalDist = 0;  // 总距离
+                var count = 0;
+                var avgDist = 0;
+                for(var i=0;i<9;++i)
+                    if(document.getElementById('D' + (i+1)*100))            // 六餐不参与
+                        { totalDist += dist[i]; ++count; }
+                avgDist = totalDist / count;
+                
+                // 距离衰减因数 = 1 - (距离/总距离) (算法一弃用)
+                // var decay = Array(9);      
+                // for(var i=0;i<9;++i)
+                //     if(document.getElementById('D' + (i+1)*100))            // 六餐不参与
+                //         decay[i] = 1 - (dist[i] / totalDist);
+                //     else
+                //         decay[i] = -1;
+
+                // 距离衰减因数 = 1 / (距离/平均距离) (算法二)
+                var decay = Array(9);      
+                for(var i=0;i<9;++i)
+                    if(document.getElementById('D' + (i+1)*100))            // 六餐不参与
+                        decay[i] = avgDist / dist[i];
+                    else
+                        decay[i] = -1;
+
+                // 剩余人数
+                var remains = Array(9);
 
                 for(i in CData){
                     
                     var id = CData[i]['Id']
+                    var shortid = id/100 - 1;
 
                     var NameControl = document.getElementById('N'+ id);
                     if(NameControl) { NameControl.innerHTML = CData[i]['Name'];}
@@ -41,11 +70,27 @@ function refreshData(){
                         seats += CData[i]['Seat_s']; //总座位数
                         used += CData[i]['Seat_u'];  //使用中的座位数
                         remain += CData[i]['Seat_r'];  //剩余的座位数
+                        remains[shortid] = CData[i]['Seat_r'];
                     }
                     var DistControl = document.getElementById('D'+ id);
-                    if(DistControl) { DistControl.innerHTML = dist[id/100 - 1];}
+                    if(DistControl) {
+                        DistControl.innerHTML = dist[shortid];
+                    }
                     
                 }
+
+                // 推荐因数
+                var recommend = Array(9);
+                for(var i=0;i<9;++i){
+                    if(document.getElementById('D' + (i+1)*100)){            // 六餐不参与
+                        recommend[i] = Math.round(remains[i] * decay[i]);
+                        var recommendControl = document.getElementById('F'+(i+1)*100);
+                        if(recommendControl) { recommendControl.innerHTML = recommend[i];}
+                    }
+                    else
+                        recommend[i] = -1;
+                }
+
 
                 document.getElementById('R000').innerHTML = remain;
                 var percent = Math.round(remain/seats * 100);
@@ -53,6 +98,7 @@ function refreshData(){
                 document.getElementById('P000').value = percent;
                 var percentctrl = document.getElementById('Q000');
                 if(percentctrl){ percentctrl.innerHTML = percent + '%'; }
+                document.getElementById('D000').innerHTML = avgDist;
             }
         };
         xmlhttp.open('GET', 'https://canteen.sjtu.edu.cn/CARD/Ajax/Place' , true);
